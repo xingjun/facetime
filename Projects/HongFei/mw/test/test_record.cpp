68f207c448e858ff2aead6c3c873973c25c473b1
agooou <agooou@gmail.com>
2011-06-27 06:26:06 +0000

    add camera record test code.

--- Projects/HongFei/mw/test/test_record.cpp
+++ Projects/HongFei/mw/test/test_record.cpp
@@ -7,7 +7,8 @@
 #endif
 
 int main () {
-
+    const char* camera_format = "video4linux2";
+    const char* camera_device = "/dev/video0";
     av_register_all ();
     avdevice_register_all();
 
@@ -17,15 +18,17 @@ int main () {
     AVCodec *decoder;
  
 /* 获取这个输入设备的处理器，以便进行处理 */
-    AVInputFormat *inputFmt = av_find_input_format ("video4linux2"); 
+    AVInputFormat *inputFmt = av_find_input_format (camera_format); 
     if (inputFmt == NULL) {
         /* 找不到这个“输入设备”，错误处理  */
+        ERROR ("can not find input deivce: %s", camera_format);
+        return -1;
     }
  
 /* 使用这个输入处理器“打开”一个文件。在Linux中，这自然是一视频设备的设备文件了 */
     memset ( &inputFmtParameter, 0, sizeof(inputFmtParameter));
-    inputFmtParameter.height = 240;
     inputFmtParameter.width  = 320;
+    inputFmtParameter.height = 240;
  
 /*
  * 在这里我们使用AVFormatParameter 这个结构，向输入处理器传递参数
@@ -33,9 +36,12 @@ int main () {
  * 你大可以什么都不传，只传NULL从而使用默认参数。
  */
  
-    if (av_open_input_file ( &fmtCtx, "/dev/video0", inputFmt,
+    if (av_open_input_file ( &fmtCtx, camera_device, inputFmt,
                              sizeof(inputFmtParameter),
                              &inputFmtParameter) < 0 ){
         /* 打开“输入设备”失败，错误处理  */
+        ERROR ("can not open device: %s", camera_device);
+        return -1;
     }
+
 }
